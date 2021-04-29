9d0c714fd03f3e171f3d26fd2faaf41d59913bee
mathieu38149 <83405336+mathieu38149@users.noreply.github.com>
2021-04-29 19:11:13 +0200

    initial commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODM3MjcxOTY0NjU3Nzc4Njg4.YIqISA.PVnXvRfqaVhPYWXhf_o0LzFw7KM',
+        prefix: '.',
+        activity: 'Fait de la musique',
+    },
+
+    filters: ['bientot'],
+};
